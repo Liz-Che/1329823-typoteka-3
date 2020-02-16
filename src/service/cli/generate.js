ed67085bab2abafb1958813ba8229a47536ddc53
Liz_CHe <alishia@list.ru>
2020-02-16 13:21:21 +0300

    Corrected what you need. For the rest, then change the conditions of the assignment.

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -1,9 +1,12 @@
 'use strict'; 
 
 const {ExitCode} = require(`../../constants`); 
-const {generateOffers, makeMock} = require(`../cli/utils`); 
+const {generateOffers} = require(`../cli/utils`); 
+const fs = require(`fs`);
 const DEFAULT_COUNT = 1; 
 const MAX_COUNT = 1000;
+const FILE_NAME = `mocks.json`;
+
 module.exports = { 
     name: `--generate`, 
     run(userIndex) { 
@@ -14,7 +17,14 @@ module.exports = {
         } else {
             const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT; 
             const content = JSON.stringify(generateOffers(countOffer));
-            makeMock(content);
+            fs.writeFileSync(FILE_NAME,content,(err) =>{
+                if (err){ 
+                    console.error(`Не удалось записать данные в файл`);
+                    process.exit(ExitCode.error);
+                }
+                console.log(`Файл записан!`);
+                process.exit(ExitCode.success);
+            });
             process.exit(ExitCode.success);
         }
     } 
--- src/service/cli/utils.js
+++ src/service/cli/utils.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const moment = require(`moment`);
-const fs = require(`fs`);
 const { TITLES,
         ANNOUNCE,
         CATEGORY,
@@ -31,22 +30,17 @@ const generateOffers = (count) => (
     Array(count).fill({}).map( () => (
         {
             title: TITLES[getReandomInt(0,TITLES.length-1)],
-            announce: shuffle(ANNOUNCE).slice(0,5).join(` `),
-            fullText: shuffle(ANNOUNCE).slice(1,getReandomInt(0, ANNOUNCE.length - 1)).join(` `),
+            //ANNOUNCE - это константа из data.js, хрянящая в себе предложения, из которых формируются уже 
+            //announce и fullText
+            // "На данном этапе не требуется, чтобы полный текст включал предложения, которые использовались в анонсе."
+            //Это цитата из задания. 
+            announce: shuffle(ANNOUNCE).slice(0,5).join(` `), //5 записей
+            //ПОЛНЫЙ ТЕКСТ - произвольное количество (это означает что от 0 до количества записей в массиве, жаль в минус уйти нельзя) и не сказано, что больше анонса, извините, но лучше тогда задание формулируйте.
+            fullText: shuffle(ANNOUNCE).slice(0,getReandomInt(0, ANNOUNCE.length - 1)).join(` `), 
             createdDate: createdRandomDate(),
             category: [CATEGORY[getReandomInt(0,CATEGORY.length-1)]],
         }))
 );
 
-const makeMock = (data) => { 
-    const FILE_NAME = `mocks.json`;
-    fs.writeFileSync(FILE_NAME,data,(err) =>{
-        if (err){ 
-            console.error(`Не удалось записать данные в файл`); 
-        }
-        console.log(`Файл записан!`);
-    });
-};
-
-module.exports = {getReandomInt, shuffle, generateOffers,makeMock};
+module.exports = {generateOffers};
 
